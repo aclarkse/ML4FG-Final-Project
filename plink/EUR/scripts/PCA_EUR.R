ddc5c1e3536b228f7ce8c30ce4879ff5d96e4971
Andrea Clark <andrea.c.sev@gmail.com>
2022-11-29 17:00:45 -0500

    added EUR processing and GWAS scripts

---
+++ plink/EUR/scripts/PCA_EUR.R
@@ -0,0 +1,46 @@
+# clear workspace
+rm(list = ls())
+
+pacman::p_load(
+  ggplot2
+)
+
+# set working directory
+setwd("/Users/clarkao1/Documents/ML4FG_project/plink/EUR")
+
+# run PCA
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_ready --pca --out pca")
+
+# read in the eigenvectors and eigenvalues files
+eigenvals <- read.table("pca.eigenval")
+eigenvecs <- read.table("pca.eigenvec")
+
+# proportion of variation captured by each eigenvec
+eigen_percent <- round((eigenvals / (sum(eigenvals)) * 100), 2)
+
+
+# pca plot
+ggplot(data = eigenvecs) + 
+  geom_point(mapping = aes(x = V3, y=V4), size = 3, show.legend = TRUE) + 
+  geom_hline(yintercept = 0, linetype = "dotted") + 
+  geom_vline(xintercept = 0, linetype = "dotted") +
+  labs(title = "PCA of European Individuals",
+       x = paste0("Principal component 1 (",eigen_percent[1,1]," %)"),
+       y = paste0("Principal component 2 (",eigen_percent[2,1]," %)")) + 
+  theme_minimal()
+
+
+# scree-plot
+ggplot(data = eigenvals) + 
+  geom_point(aes(x=c(1:20), y=V1), size=3) + 
+  geom_line(aes(x=c(1:20), y=V1)) + 
+  geom_vline(xintercept = 5, linetype = "dashed") +
+  labs(title="Scree Plot",
+       subtitle = "European Individuals",
+       x = "Component",
+       y = "Explained Variance") +
+  theme(
+    plot.title = element_text(color = "black", size = 14, face = "bold"),
+    plot.subtitle = element_text(color = "black", size = 10))
+
+    
---
+++ plink/EUR/scripts/qc_EUR.R
@@ -0,0 +1,57 @@
+# clear workspace
+rm(list = ls())
+
+pacman::p_load(
+  rrBLUP,
+  BGLR,
+  DT,
+  SNPRelate,
+  dyplr,
+  qqman,
+  poolR
+)
+
+
+# set working directory
+setwd("/Users/clarkao1/Documents/ML4FG_project/plink/EUR")
+
+# Quality control criteria
+## Missingness per SNP: 0.1 --geno
+## Missingness per individual: 0.1 --mind
+## Minor allele frequency: 0.01 --maf
+## Hardy-Weinberg threshold: 0.0000001 -hwe
+######################################
+# Plink Quality Control #
+######################################
+# make a binary file
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --make-bed --out EUR") 
+
+# look at frequencies
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --freq --out EUR_freq")
+
+# check for Hardy-Weinberg disequilibrium
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --hardy --out EUR_hardy")
+
+# genotyping rate per individual and per marker
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --missing --out EUR_missing")
+
+# check for differential genotyping rate -- skipped as at least one case and one control needed
+#system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --test-missing --out EUR_test")
+
+# add phenotype information
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR --pheno pheno.txt --make-bed --out EUR_pheno")
+
+# filter for missingness in data
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_pheno --maf 0.01 --geno 0.01 --mind 0.01 --hwe 0.0000001 --hwe-all --out EUR_filtered --make-bed")
+
+# filter for only autosomes
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_filtered --chr 1-22 --out EUR_auto --make-bed")
+
+# check for relationships between individuals with pihat > 0.2 (crytic relatedness)
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_auto --genome --min 0.2 --out EUR_crytic --make-bed")
+
+# make binary files for prepared data
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_crytic --out EUR_ready --make-bed")
+
+# make .ped and .map files for prepared data
+system("/Users/clarkao1/Documents/ML4FG_project/plink/plink --bfile EUR_crytic --out EUR_ready --recode")
